b849a5e847448f87bc97d8eb9a708351fa97086b
Rodrick1234 <lolpeople.mega@gmail.com>
2024-11-07 16:54:01 -0400

    Código
    Código completo!

--- src/fakedb/CursoFakeDb.java
+++ src/fakedb/CursoFakeDb.java
@@ -4,19 +4,25 @@ import java.util.ArrayList;
 
 import dominio.Curso;
 
-public class CursoFakeDb extends BaseFakeDb<Curso>
-{
+public class CursoFakeDb extends BaseFakeDb<Curso> {
 
     @Override
-    public ArrayList<Curso> PreencherDados()
-    {
+    public ArrayList<Curso> PreencherDados() {
         this.tabela = new ArrayList<>();
-        this.tabela.add(new Curso(1, "C. Computação", 
-        "Curso dos cria",40));
+        this.tabela.add(new Curso(1, "C. Computação - BACHARELADO",
+                "Curso dos Programadores", 800));
+
+        this.tabela.add(new Curso(2, "Medicina",
+                "Curso dos Médicos", 1000));
+
+        this.tabela.add(new Curso(3, "Moda",
+                "Curso dos que se vestem bem", 10));
+
         return this.tabela;
+
     }
-    public CursoFakeDb()
-    {
+
+    public CursoFakeDb() {
         super();
     }
 }
