e9a7cd547627d4db044a00d7312090557705d368
Rodrick1234 <lolpeople.mega@gmail.com>
2024-11-07 17:06:31 -0400

    Código
    Código Atualizado!!

--- src/menu/CursoMenu.java
+++ src/menu/CursoMenu.java
@@ -4,31 +4,45 @@ import java.util.ArrayList;
 import dominio.Curso;
 import servico.CursoServico;
 
-public class CursoMenu extends BaseMenu {
+public class CursoMenu extends BaseMenu
+{
 
     private CursoServico srv;
 
-    public CursoMenu() {
+    public CursoMenu()
+    {
         super();
         this.srv = new CursoServico();
     }
 
     @Override
-    public void ExibirMenu() {
-        int opcao = 0;
-        while (opcao != 9) {
+    public void ExibirMenu()
+    {
+        int opcao = 0, once = 0;
+        while (opcao != 9)
+        {
             Util.ClearConsole();
+            if (once == 0)
+            {
+                System.out.println("Trabalho de POO1 - Atividade 2");
+                System.out.println("");
+                once = 1;
+            }
             System.out.println("Menu de Opções");
+            System.out.println("");
             System.out.println("1 - Listar");
             System.out.println("2 - Localizar");
             System.out.println("3 - Adicionar");
             System.out.println("4 - Atualizar");
             System.out.println("5 - Remover");
+            System.out.println("");
             System.out.println("9 - Sair");
-            System.out.print("Digite sua opção: ");
+            System.out.println("");
+            System.out.print("Digite sua opção... ");
             opcao = this.scanner.nextInt();
             scanner.nextLine();
-            switch (opcao) {
+            switch (opcao)
+            {
                 case 1:
                     this.Listar();
                     break;
@@ -46,60 +60,73 @@ public class CursoMenu extends BaseMenu {
                     break;
                 case 9:
                     System.out.println("Saindo...");
+                    System.out.println("");
                     break;
                 default:
-                    System.out.println("Opção inválida");
+                    System.out.println("Opção inválida!");
                     break;
             }
         }
     }
 
     @Override
-    public void Listar() {
+    public void Listar()
+    {
         Util.ClearConsole();
         System.out.println("Listando...");
+        System.out.println("");
 
         ArrayList<Curso> lista = this.srv.LerTodos();
-        for (Curso curso : lista) {
+        for (Curso curso : lista)
+        {
             this.ImprimirEmLinha(curso);
         }
 
-        System.out.println("Clique <ENTER> para continuar...");
+        System.out.println("");
+        System.out.println("Clique <ENTER> para continuar... ");
         this.scanner.nextLine();
     }
 
     @Override
-    public void Localizar() {
+    public void Localizar()
+    {
         Util.ClearConsole();
         System.out.println("Localizando...");
+        System.out.println("");
 
-        System.out.print("Informe o código do Curso: ");
+        System.out.print("Informe o código do Curso... ");
         int codigo = this.scanner.nextInt();
         scanner.nextLine();
 
         Curso curso = this.srv.Ler(codigo);
-        if (curso != null) {
+        if (curso != null)
+        {
             this.ImprimirEmLinha(curso);
-        } else {
-            System.out.println("Problema - Curso não foi encontrado");
+        }
+        else
+        {
+            System.out.println("Problema - Curso não foi encontrado!");
         }
 
-        System.out.println("Clique <ENTER> para continuar...");
+        System.out.println("");
+        System.out.println("Clique <ENTER> para continuar... ");
         this.scanner.nextLine();
     }
 
     @Override
-    public void Adicionar() {
+    public void Adicionar()
+    {
         Util.ClearConsole();
         System.out.println("Adicionando...");
+        System.out.println("");
 
-        System.out.print("Informe o Nome do Curso: ");
+        System.out.print("Informe o Nome do Curso... ");
         String nome = this.scanner.nextLine();
 
-        System.out.print("Informe a Descrição do Curso: ");
+        System.out.print("Informe a Descrição do Curso... ");
         String descricao = this.scanner.nextLine();
 
-        System.out.print("Informe a Carga Horária do Curso: ");
+        System.out.print("Informe a Carga Horária do Curso... ");
         int cargahr = this.scanner.nextInt();
 
         this.scanner.nextLine();
@@ -111,68 +138,91 @@ public class CursoMenu extends BaseMenu {
         curso.setCargahr(cargahr);
 
         Curso cursoNovo = this.srv.Criar(curso);
-        if (cursoNovo != null) {
-            System.out.println("Curso adicionado com sucesso");
-        } else {
-            System.out.println("Problema - Curso não foi adicionado");
+        if (cursoNovo != null)
+        {
+            System.out.println("Curso adicionado com sucesso!");
+        }
+        else
+        {
+            System.out.println("Problema - Curso não foi adicionado!");
         }
 
-        System.out.println("Clique <ENTER> para continuar...");
+        System.out.println("");
+        System.out.println("Clique <ENTER> para continuar... ");
         this.scanner.nextLine();
     }
 
     @Override
-    public void Atualizar() {
+    public void Atualizar()
+    {
         Util.ClearConsole();
         System.out.println("Atualizando...");
+        System.out.println("");
 
-        System.out.print("Informe o código do Curso: ");
+        System.out.print("Informe o código do Curso... ");
         int codigo = this.scanner.nextInt();
         scanner.nextLine();
 
         Curso curso = this.srv.Ler(codigo);
-        if (curso != null) {
-            System.out.print("Informe a nova Descrição para este Curso: ");
+        if (curso != null)
+        {
+            System.out.print("Informe a nova Descrição para este Curso... ");
             String descricao = this.scanner.nextLine();
             curso.setDescricao(descricao);
-            if (this.srv.Atualizar(curso) != null) {
-                System.out.println("Curso atualizado com sucesso");
-            } else {
-                System.out.println("Problema - Curso não foi atualizado");
+            if (this.srv.Atualizar(curso) != null)
+            {
+                System.out.println("Curso atualizado com sucesso!");
             }
-        } else {
-            System.out.println("Problema - Curso não foi encontrado");
+            else
+            {
+                System.out.println("Problema - Curso não foi atualizado!");
+            }
+        }
+        else
+        {
+            System.out.println("Problema - Curso não foi encontrado!");
         }
 
-        System.out.println("Clique <ENTER> para continuar...");
+        System.out.println("");
+        System.out.println("Clique <ENTER> para continuar... ");
         this.scanner.nextLine();
     }
 
     @Override
-    public void Remover() {
+    public void Remover()
+    {
         Util.ClearConsole();
         System.out.println("Removendo...");
+        System.out.println("");
 
-        System.out.print("Informe o código do Curso: ");
+        System.out.print("Informe o código do Curso... ");
         int codigo = this.scanner.nextInt();
         scanner.nextLine();
 
         Curso curso = this.srv.Ler(codigo);
-        if (curso != null) {
-            if (this.srv.Deletar(codigo) != null) {
-                System.out.println("Curso removido com sucesso");
-            } else {
-                System.out.println("Problema - Curso não foi removido");
+        if (curso != null)
+        {
+            if (this.srv.Deletar(codigo) != null)
+            {
+                System.out.println("Curso removido com sucesso!");
             }
-        } else {
-            System.out.println("Problema - Curso não foi encontrado");
+            else
+            {
+                System.out.println("Problema - Curso não foi removido!");
+            }
+        }
+        else
+        {
+            System.out.println("Problema - Curso não foi encontrado!");
         }
 
-        System.out.println("Clique <ENTER> para continuar...");
+        System.out.println("");
+        System.out.println("Clique <ENTER> para continuar... ");
         this.scanner.nextLine();
     }
 
-    private void ImprimirEmLinha(Curso curso) {
+    private void ImprimirEmLinha(Curso curso)
+    {
         String msg = "Curso - ";
         msg += "Código: " + curso.getCodigo() + " | ";
         msg += "Nome: " + curso.getNome() + " | ";
